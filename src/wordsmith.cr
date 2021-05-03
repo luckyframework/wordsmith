e62382e554e2b002d3912f2d2ed4ce162e2f9659
Stephen Dolan <dolan.stephen1@gmail.com>
2021-05-03 12:13:27 -0400

    Add missing inline documentation (#16)

--- src/wordsmith.cr
+++ src/wordsmith.cr
@@ -1,6 +1,8 @@
 require "./wordsmith/**"
 require "./wordsmith/inflector/**"
 
+# Wordsmith is a library for pluralizing, ordinalizing, singularizing and doing other fun and useful things with words.
 module Wordsmith
-  VERSION = "0.2.2"
+  # The current Wordsmith version is defined in `shard.yml`
+  VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
 end
--- src/wordsmith/inflector/methods.cr
+++ src/wordsmith/inflector/methods.cr
@@ -4,14 +4,39 @@ module Wordsmith
   module Inflector
     extend self
 
+    # Given a word, return the plural version of that word.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.pluralize("sandal") # => "sandals"
+    # Wordsmith::Inflector.pluralize("person") # => "people"
+    # Wordsmith::Inflector.pluralize("people") # => "people"
+    # ```
     def pluralize(word)
       apply_inflections(word, inflections.plurals)
     end
 
+    # Given a word, return the singular version of that word.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.singularize("sandals") # => "sandal"
+    # Wordsmith::Inflector.singularize("people")  # => "person"
+    # Wordsmith::Inflector.singularize("person")  # => "person"
+    # ```
     def singularize(word)
       apply_inflections(word, inflections.singulars)
     end
 
+    # Convert a given word to the camel-case version of that word.
+    #
+    # Optionally, a second parameter can be provided that controls whether or not the first letter is capitalized.
+    #
+    # Examples:
+    # ```
+    # Wordsmith::Inflector.camelize("application_controller")                                # => "ApplicationController"
+    # Wordsmith::Inflector.camelize("application_controller", uppercase_first_letter: false) # => "applicationController"
+    # ```
     def camelize(term, uppercase_first_letter = true)
       string = term.to_s
       string = if uppercase_first_letter
@@ -30,6 +55,12 @@ module Wordsmith
       string
     end
 
+    # Convert a given camel-case word to the underscored version of that word.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.underscore("ApplicationController") # => "application_controller"
+    # ```
     def underscore(camel_cased_word)
       return camel_cased_word unless camel_cased_word =~ /[A-Z-]|::/
       word = camel_cased_word.to_s.gsub("::", "/")
@@ -43,6 +74,16 @@ module Wordsmith
       word
     end
 
+    # Convert a given word to the human-friendly version of that word.
+    #
+    # Capitalization and whether or not to retain an `_id` suffix can be controlled with optional parameters.
+    #
+    # Examples:
+    # ```
+    # Wordsmith::Inflector.humanize("employee_id")                       # => "Employee"
+    # Wordsmith::Inflector.humanize("employee_id", capitalize: false)    # => "employee"
+    # Wordsmith::Inflector.humanize("employee_id", keep_id_suffix: true) # => "Employee id"
+    # ```
     def humanize(lower_case_and_underscored_word, capitalize = true, keep_id_suffix = false)
       result = lower_case_and_underscored_word.to_s.dup
 
@@ -70,29 +111,69 @@ module Wordsmith
       result
     end
 
+    # Capitalize the first letter of a given word.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.upcase_first("lucky") # => "Lucky"
+    # ```
     def upcase_first(string)
       string.size > 0 ? string[0].to_s.upcase + string[1..-1] : ""
     end
 
+    # Convert a given word to the titleized version of that word, which generally means each word is capitalized.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.titleize("amazon web services") # => "Amazon Web Services"
+    # ```
     def titleize(word, keep_id_suffix = false)
       humanize(underscore(word), keep_id_suffix: keep_id_suffix).gsub(/\b(?<!\w['’`])[a-z]/) do |match|
         match.capitalize
       end
     end
 
+    # Convert a given class name to the database table name for that class.
+    #
+    # Examples:
+    # ```
+    # Wordsmith::Inflector.tableize("User")   # => "users"
+    # Wordsmith::Inflector.tableize("Person") # => "people"
+    # ```
     def tableize(class_name)
       pluralize(underscore(class_name))
     end
 
+    # Convert a given table name to the class name for that table.
+    #
+    # Examples:
+    # ```
+    # Wordsmith::Inflector.classify("users")               # => "User"
+    # Wordsmith::Inflector.classify("people")              # => "Person"
+    # Wordsmith::Inflector.classify("schema.users")        # => "User"
+    # Wordsmith::Inflector.classify("schema.public.users") # => "User"
+    # ```
     def classify(table_name)
       # strip out any leading schema name
       camelize(singularize(table_name.to_s.sub(/.*\./, "")))
     end
 
+    # Convert a given underscore-separated word to the same word, separated by dashes.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.dasherize("post_office") # => "post-office"
+    # ```
     def dasherize(underscored_word)
       underscored_word.tr("_", "-")
     end
 
+    # Remove leading modules from a provided class name path.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.demodulize("Helpers::Mixins::User") # => "User"
+    # ```
     def demodulize(path)
       path = path.to_s
       if i = path.rindex("::")
@@ -102,14 +183,35 @@ module Wordsmith
       end
     end
 
+    # Remove any trailing constants from the provided path.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.deconstantize("Helpers::Mixins::User::FREE_TIER_COMMENTS") # => "Helpers::Mixins::User"
+    # ```
     def deconstantize(path)
       path.to_s[0, path.rindex("::") || 0] # implementation based on the one in facets' Module#spacename
     end
 
+    # Determine the foreign key representation of a given class name.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.foreign_key("Person") # => "person_id"
+    # ```
     def foreign_key(class_name, separate_class_name_and_id_with_underscore = true)
       underscore(demodulize(class_name)) + (separate_class_name_and_id_with_underscore ? "_id" : "id")
     end
 
+    # Determine the ordinal suffix for a given number.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.ordinal(1) # => "st"
+    # Wordsmith::Inflector.ordinal(2) # => "nd"
+    # Wordsmith::Inflector.ordinal(3) # => "rd"
+    # Wordsmith::Inflector.ordinal(4) # => "th"
+    # ```
     def ordinal(number)
       abs_number = number.to_i.abs
 
@@ -125,10 +227,29 @@ module Wordsmith
       end
     end
 
+    # Given a number, return the number with the correct ordinal suffix.
+    #
+    # Example:
+    # ```
+    # Wordsmith::Inflector.ordinalize(1) # => "1st"
+    # Wordsmith::Inflector.ordinalize(2) # => "2nd"
+    # Wordsmith::Inflector.ordinalize(3) # => "3rd"
+    # Wordsmith::Inflector.ordinalize(4) # => "4th"
+    # ```
     def ordinalize(number)
       "#{number}#{ordinal(number)}"
     end
 
+    # Convert the given string to a parameter-friendly version.
+    #
+    # The used separator and whether or not to preserve the original object case can be controlled through optional parameters.
+    #
+    # Examples:
+    # ```
+    # Wordsmith::Inflector.parameterize("Admin/product")                       # => "admin-product"
+    # Wordsmith::Inflector.parameterize("Admin::Product", separator: "_")      # => "admin_product"
+    # Wordsmith::Inflector.parameterize("Admin::Product", preserve_case: true) # => "Admin-Product"
+    # ```
     def parameterize(content : String, separator : String? = "-", preserve_case : Bool = false)
       parameterized_string = content.gsub(/[^a-z0-9\-_]+/i, separator)
 
@@ -149,6 +270,7 @@ module Wordsmith
       parameterized_string
     end
 
+    # Apply the previously-defined inflection rules to a given word.
     private def apply_inflections(word, rules)
       result = word.to_s.dup
 
